c503552e1c3c930a2c23dcbcccee4ca2868ce3a9
Mateusz Siga <sigamateusz@gmail.com>
2016-11-30 13:07:31 +0100

    added print_decoded(i, n)

--- decoder.py
+++ decoder.py
@@ -1,7 +1,7 @@
-ALPHABET = [['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
-            'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'],
-            ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
-            'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']]
+ALPHABET = (('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+            'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'),
+            ('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+            'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'))
 
 
 def content_to_str(data_file_name):
@@ -10,6 +10,11 @@ def content_to_str(data_file_name):
     with open(data_file_name, 'r') as f:
         content = f.read()
     return content
+
+
+def print_decoded(i, n):
+    poz = ALPHABET[n].index(i) + 1
+    print(ALPHABET[n][-poz], end='')
 
 
 def main():
@@ -21,11 +26,9 @@ def main():
         elif i == " ":
             print(' ', end='')
         elif i in ALPHABET[0]:
-                poz = ALPHABET[0].index(i) + 1
-                print(ALPHABET[0][-poz], end='')
+            print_decoded(i, 0)
         elif i in ALPHABET[1]:
-                poz = ALPHABET[1].index(i) + 1
-                print(ALPHABET[1][-poz], end='')
+            print_decoded(i, 1)
 
 
 if __name__ == '__main__':
